f1b2fbc3e9ab463eb75bb0ba4052ac51aaeb1788
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-03 13:52:27 +0100

    Inkrement

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+// arithmetische Operatioren
+
+let score = 5 + 5 * 10;
+console.log(score)
+
+score = (5+5)*10
+console.log(score)
+
+score = 0
+console.log(score) // 0
+
+score = score + 10 // 10
+console.log(score)
+
+score += 10
+console.log(score) //20
+
+
+let zahl = 1
+console.log(zahl) // 1
+zahl = zahl +1
+console.log(zahl) // 2
+zahl += 2
+console.log(zahl) // 4
+
+
+// Inkrement-Operatoren
+
+zahl ++
+console.log(zahl) // 5
+
+// Dekrement-Operatioren
+zahl -- 
+console.log(zahl)  // 4
